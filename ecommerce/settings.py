dfc4850bc131b76b93566b0d60b72c4aec19ebf4
Sair Ali <sairali7037@gmail.com>
2025-05-03 23:51:23 +0530

    Email Update setting

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 #sending emails
 
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='Enter your mail'
-EMAIL_HOST_PASSWORD='Enter Password'
+EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_USE_SSL=False
