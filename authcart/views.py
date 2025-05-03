dc19c07a791bcb7b94f339446b7eb6530b0eaa98
Sair Ali <sairali7037@gmail.com>
2025-05-03 23:57:32 +0530

    domain update3.0

--- authcart/views.py
+++ authcart/views.py
@@ -36,7 +36,7 @@ def signup(request):
         message=render_to_string('activate.html',{
             'user':user,
             # 'domain':'127.0.0.1:8000',
-            'domain':'127.0.0.1:8000',
+            'domain':'ecommerce-shopcart.onrender.com',
             'uid':urlsafe_base64_encode(force_bytes(user.pk)),
             'token':generate_token.make_token(user)
         })
@@ -98,7 +98,7 @@ class RequestResetEmailView(View):
             # current_site=get_current_site(request)
             email_subject='[Reset Your Password]'
             message=render_to_string('reset-user-password.html',{
-                'domain':'127.0.0.1:8000',
+                'domain':'ecommerce-shopcart.onrender.com',
                 'uid':urlsafe_base64_encode(force_bytes(user[0].pk)),
                 'token':PasswordResetTokenGenerator().make_token(user[0])
             })
